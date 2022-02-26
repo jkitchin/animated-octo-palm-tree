241a8c31959d13bbc06d56e8e3cfad21ac408221
John Kitchin <jkitchin@andrew.cmu.edu>
2022-02-26 16:06:34 -0500

    add new problem

--- tests.el
+++ tests.el
@@ -4,3 +4,14 @@
 (ert-deftest answer-a ()
   (should (string= "5" (with-current-buffer (find-file-noselect "quiz.org")
 			 (nth 1 (car (org-collect-keywords '("ANSWER-A"))))))))
+
+
+(ert-deftest answer-b ()
+  (should (string= "42"
+		   (with-current-buffer (find-file-noselect "quiz.org")
+		     (org-babel-goto-named-result "answer-b")
+		     (let ((result (org-element-context)))
+		       (string-trim
+			(buffer-substring-no-properties
+			 (org-element-property :contents-begin result)
+			 (org-element-property :contents-end result))))))))
